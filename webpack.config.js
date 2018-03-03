5bb1993f82e1620adea94e684d44f48e4d4b04c5
Haruna <haruna0510@kif.biglobe.ne.jp>
2018-03-03 17:10:50 +0900

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,37 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack')
+const path = require('path')
+const SplitChunksPlugin = require('webpack-split-chunks')
+
+const VENDOR_LIBS = [
+    'react', 'lodash', 'redux', 'react-redux', 'react-dom',
+    'faker', 'react-input-range', 'redux-form', 'redux-thunk'
+]
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    entry: {
+        bundle: './src/index.js',  // two entry points
+    }
+    ,
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].[chunkhash].js'
+    },
+    module: {
+        rules: [
+            {
+                use: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/  // No need to compile because it's ES5
+            }, {
+                use: ['style-loader', 'css-loader'],   // understand the contents of css, and put the code into index.html
+                test: /\.css$/
+            }
+        ]
+    },
+    plugins: [
+        new SplitChunksPlugin({
+            to: 'vendor',
+            test: /node_modules/
+        })
+    ]
 };
